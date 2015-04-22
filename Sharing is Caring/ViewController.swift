d9f565273dda53370bceec158011cc71f1e77685
Jure Žove <jure.pase.kure@gmail.com>
2015-04-22 17:28:54 +0200

    Initial commit

--- Sharing is Caring/ViewController.swift
+++ Sharing is Caring/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.welcomeLabel.text = SharingManager.sharedInstance.welcomeMessage
     }
-
-
 }
 
